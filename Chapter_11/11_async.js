22b4fa152f0c3bf304e361e094e8010669e9d4f6
Sotiris Koukios-Panopoulos <sokopa@gmail.com>
2020-04-25 19:05:36 +0300

    Chapter 11 - Exercise 1

--- Chapter_11/11_async.js
+++ Chapter_11/11_async.js
@@ -186,3 +186,26 @@ async function chicks(nest, year) {
   );
   return list;
 }
+
+async function locateScalpel(nest) {
+  // Your code here.
+  let current = nest.name;
+  while (true) {
+    let next = await anyStorage(nest, current, 'scalpel');
+    if (next == current) return current;
+    current = next;
+  }
+}
+
+function locateScalpel2(nest) {
+  function loop(current) {
+    return anyStorage(nest, current, 'scalpel').then((next) => {
+      if (next == current) return current;
+      else return loop(next);
+    });
+  }
+  return loop(nest.name);
+}
+
+locateScalpel2(bigOak).then(console.log);
+// → Butcher Shop
