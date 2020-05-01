bce863198547f45d2bcadb957ee359db1b5b8bd4
Sotiris Koukios-Panopoulos <sokopa@gmail.com>
2020-05-01 13:05:42 +0300

    Chapter 16 - Exercise 3

--- Chapter_16/code/chapter/16_game.js
+++ Chapter_16/code/chapter/16_game.js
@@ -100,6 +100,40 @@ var Lava = class Lava {
 
 Lava.prototype.size = new Vec(1, 1);
 
+const monsterSpeed = 5;
+var Monster = class Monster {
+  constructor(pos) {
+    this.pos = pos;
+  }
+
+  get type() {
+    return 'monster';
+  }
+
+  static create(pos) {
+    return new Monster(pos.plus(new Vec(0, -1)));
+  }
+
+  update(time, state) {
+    let player = state.player;
+    let speed = (player.pos.x < this.pos.x ? -1 : 1) * time * monsterSpeed;
+    let newPos = new Vec(this.pos.x + speed, this.pos.y);
+    if (state.level.touches(newPos, this.size, 'wall')) return this;
+    else return new Monster(newPos);
+  }
+
+  collide(state) {
+    let player = state.player;
+    if (player.pos.y + player.size.y < this.pos.y + 0.5) {
+      let filtered = state.actors.filter((a) => a != this);
+      return new State(state.level, filtered, state.status);
+    } else {
+      return new State(state.level, state.actors, 'lost');
+    }
+  }
+};
+Monster.prototype.size = new Vec(1.2, 2);
+
 var Coin = class Coin {
   constructor(pos, basePos, wobble) {
     this.pos = pos;
@@ -128,6 +162,7 @@ var levelChars = {
   '=': Lava,
   '|': Lava,
   v: Lava,
+  M: Monster,
 };
 
 var simpleLevel = new Level(simpleLevelPlan);
