c83b85a5ddb661c1f2ce55ef239884d0f361a9af
Sotiris Koukios-Panopoulos <sokopa@gmail.com>
2020-05-03 12:33:04 +0300

    Chapter 19 - Paint - ex1

--- Chapter)19/code/main.js
+++ Chapter)19/code/main.js
@@ -124,12 +124,28 @@ class PixelEditor {
     this.controls = controls.map((Control) => new Control(state, config));
     this.dom = elt(
       'div',
-      {},
+      { tabIndex: 0, onkeydown: (event) => this.keyDown(event, config) },
       this.canvas.dom,
       elt('br'),
       ...this.controls.reduce((a, c) => a.concat(' ', c.dom), [])
     );
   }
+
+  keyDown(event, config) {
+    if (event.key == 'z' && (event.ctrlKey || event.metaKey)) {
+      event.preventDefault();
+      config.dispatch({ undo: true });
+    } else if (!event.ctrlKey && !event.metaKey && !event.altKey) {
+      for (let tool of Object.keys(config.tools)) {
+        if (tool[0] == event.key) {
+          event.preventDefault();
+          config.dispatch({ tool });
+          return;
+        }
+      }
+    }
+  }
+
   syncState(state) {
     this.state = state;
     this.canvas.syncState(state.picture);
